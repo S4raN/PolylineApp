a36dae9fb75e34ea8a37e3899bd5c747bed2a973
Ilya <113358744+S4raN@users.noreply.github.com>
2024-05-28 10:28:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GraphPlotter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.ListBox listBoxGraphs;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnPlot;
+        private System.Windows.Forms.NumericUpDown numericUpDownX0;
+        private System.Windows.Forms.NumericUpDown numericUpDownY0;
+        private System.Windows.Forms.Label labelX0;
+        private System.Windows.Forms.Label labelY0;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBoxGraphs = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnPlot = new System.Windows.Forms.Button();
+            this.numericUpDownX0 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownY0 = new System.Windows.Forms.NumericUpDown();
+            this.labelX0 = new System.Windows.Forms.Label();
+            this.labelY0 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart.Location = new System.Drawing.Point(12, 12);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(776, 426);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // listBoxGraphs
+            // 
+            this.listBoxGraphs.FormattingEnabled = true;
+            this.listBoxGraphs.Location = new System.Drawing.Point(12, 444);
+            this.listBoxGraphs.Name = "listBoxGraphs";
+            this.listBoxGraphs.Size = new System.Drawing.Size(200, 95);
+            this.listBoxGraphs.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 444);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 444);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(350, 444);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // btnPlot
+            // 
+            this.btnPlot.Location = new System.Drawing.Point(416, 444);
+            this.btnPlot.Name = "btnPlot";
+            this.btnPlot.Size = new System.Drawing.Size(75, 23);
+            this.btnPlot.TabIndex = 5;
+            this.btnPlot.Text = "build";
+            this.btnPlot.UseVisualStyleBackColor = true;
+            this.btnPlot.Click += new System.EventHandler(this.btnPlot_Click);
+            // 
+            // numericUpDownX0
+            // 
+            this.numericUpDownX0.Location = new System.Drawing.Point(218, 483);
+            this.numericUpDownX0.Name = "numericUpDownX0";
+            this.numericUpDownX0.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownX0.TabIndex = 6;
+            this.numericUpDownX0.ValueChanged += new System.EventHandler(this.numericUpDownX0_ValueChanged);
+            // 
+            // numericUpDownY0
+            // 
+            this.numericUpDownY0.Location = new System.Drawing.Point(324, 483);
+            this.numericUpDownY0.Name = "numericUpDownY0";
+            this.numericUpDownY0.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownY0.TabIndex = 7;
+            // 
+            // labelX0
+            // 
+            this.labelX0.AutoSize = true;
+            this.labelX0.Location = new System.Drawing.Point(218, 467);
+            this.labelX0.Name = "labelX0";
+            this.labelX0.Size = new System.Drawing.Size(74, 13);
+            this.labelX0.TabIndex = 8;
+            this.labelX0.Text = "Начало X (x0)";
+            // 
+            // labelY0
+            // 
+            this.labelY0.AutoSize = true;
+            this.labelY0.Location = new System.Drawing.Point(321, 467);
+            this.labelY0.Name = "labelY0";
+            this.labelY0.Size = new System.Drawing.Size(74, 13);
+            this.labelY0.TabIndex = 9;
+            this.labelY0.Text = "Начало Y (y0)";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.labelY0);
+            this.Controls.Add(this.labelX0);
+            this.Controls.Add(this.numericUpDownY0);
+            this.Controls.Add(this.numericUpDownX0);
+            this.Controls.Add(this.btnPlot);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBoxGraphs);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY0)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GraphPlotter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавляем названия графиков в ListBox
+            listBoxGraphs.Items.Add("y = k * ln(x + b)");
+            listBoxGraphs.Items.Add("y = ax^2 + bx + c");
+            listBoxGraphs.Items.Add("y = k * sin(x + b)");
+
+            // Настройка Chart
+            chart.Series.Clear();
+            chart.ChartAreas.Add(new ChartArea("MainArea"));
+        }
+
+        private void btnPlot_Click(object sender, EventArgs e)
+        {
+            // Очистка графика
+            chart.Series.Clear();
+
+            // Получение выбранного графика
+            int selectedIndex = listBoxGraphs.SelectedIndex;
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("Пожалуйста, выберите график из списка.");
+                return;
+            }
+
+            // Получение и проверка коэффициентов
+            if (!double.TryParse(textBox1.Text, out double coef1) ||
+                !double.TryParse(textBox2.Text, out double coef2) ||
+                !double.TryParse(textBox3.Text, out double coef3))
+            {
+                MessageBox.Show("Пожалуйста, введите корректные коэффициенты.");
+                return;
+            }
+
+            // Получение и проверка начальных координат
+            double x0 = (double)numericUpDownX0.Value;
+            double y0 = (double)numericUpDownY0.Value;
+
+            // Построение графика
+            Series series = new Series("Graph");
+            series.ChartType = SeriesChartType.Line;
+
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                double y = 0;
+                switch (selectedIndex)
+                {
+                    case 0:
+                        y = coef1 * Math.Log(x + coef2);
+                        break;
+                    case 1:
+                        y = coef1 * x * x + coef2 * x + coef3;
+                        break;
+                    case 2:
+                        y = coef1 * Math.Sin(x + coef2);
+                        break;
+                }
+
+                // Применение сдвига начала координат
+                x += x0;
+                y += y0;
+
+                series.Points.AddXY(x, y);
+            }
+
+            chart.Series.Add(series);
+        }
+
+        private void numericUpDownX0_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
